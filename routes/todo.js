f052f8648485eafd1683fa83b559e175e0b014b6
jmramirez <josemramirez81@outlook.com>
2020-06-24 17:00:27 -0400

    POST/PUT/PATCH Routes

--- routes/todo.js
+++ routes/todo.js
@@ -35,5 +35,48 @@ module.exports = [
             if( todoList[id]) return h.response(todoList[id])
             return h.response({message: 'Not found'}).code(404)
         }
+    },
+    {
+      method: 'POST',
+      path: '/todo',
+      handler: (request, h) => {
+          const todo = request.payload;
+          todoList.push(todo);
+          return h.response({message: 'created'})
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/todo/{id}',
+        handler: (request, h) => {
+            const index = request.params.id -1;
+            // replace the whole resource with the new one
+            todoList[index] = request.payload;
+            return h.response({ message: 'updated'})
+        }
+    },
+    {
+        method: 'PATCH',
+        path: '/todo/{id}',
+        handler: (request, h) => {
+            const index = request.params.id -1;
+            // replace the whole resource with the new one
+            const todo = todoList[index]
+            Object.keys(request.payload).forEach(key => {
+                if(key in todo) {
+                    todo[key] = request.payload[key]
+                }
+            })
+            return h.response({ message: 'patched'})
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/todo/{id}',
+        handler: (request, h) => {
+            const index = request.params.id -1
+            delete todoList[index]
+            return h.response({ message: 'deleted'});
+        }
     }
 ]
